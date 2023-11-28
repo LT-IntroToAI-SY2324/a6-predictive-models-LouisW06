be0e76111415ae31de19c5183fc1246f5ae319df
Louis Woodley <lwoodley@cps.edu>
2023-11-28 11:19:45 -0600

    part 1

--- part1-linear-regression/a6_part1.py
+++ part1-linear-regression/a6_part1.py
@@ -8,17 +8,14 @@ x = data["Age"].values
 y = data["Blood Pressure"].values
 
 # Use reshape to turn the x values into 2D arrays:
-x = x.reshape(-1,1)
+x = x.reshape(-1, 1)
 
 # Create the model
-plt.figure(figsize=(12,8))
-plt.scatter(x,y)
-plt.xlabel("Age")
-plt.ylabel("Blood Pressure")
-plt.title("Average blood pressure ")
+model = LinearRegression().fit(x, y)
+
+
 # Find the coefficient, bias, and r squared values. 
 # Each should be a float and rounded to two decimal places. 
-model = LinearRegression().fit(x, y)
 coef = round(float(model.coef_), 2)
 intercept = round(float(model.intercept_), 2)
 r_squared = model.score(x, y)
@@ -31,5 +28,16 @@ print(f"R Squared value: {r_squared}")
 x_predict = 43
 prediction = model.predict([[x_predict]])
 print(f"Prediction when x is {x_predict}: {prediction}")
+
+plt.figure(figsize=(12,8))
+plt.scatter(x,y)
+plt.xlabel("Age")
+plt.ylabel("Blood Pressure")
+plt.title("Average blood pressure ")
+plt.scatter(x,y, c="purple")
+plt.scatter(x_predict, prediction, c="blue")
+
 # Create the model in matplotlib and include the line of best fit
 plt.plot(x, coef*x + intercept, c="r", label="Line of Best Fit")
+
+plt.show()
--- part1-linear-regression/part1_writeup.md
+++ part1-linear-regression/part1_writeup.md
@@ -6,6 +6,12 @@ After completing `a6_part1.py` answer the following questions
 
 1. What is the r squared value?  What does this say about this lenear regression model?
 
+The r squared value is how accurate your line of best fit is to the data on the graph.
+
 2. According to your model, what is the predicted systolic blood pressure for a person iwho is 43 years old?
 
+For somebody who is 43 years old, the predicted blood pressure is around 137.
+
 3. How accurate do you think this model's predictions are?  Do you think this model is accurate enough to reliably predict someone's blood pressure based on their age?  Why or why not?  And if not, what could improve the model?
+
+I would not say that this model is accurate enough to reliably predict someone's bood pressure for a few reasons. The first is because this model was made by high school students and not medical professionals, and the second is because blood pressure is not solely predicted by age and can also be affected by other factors such as diet, meaning a linear model would not be effective.
