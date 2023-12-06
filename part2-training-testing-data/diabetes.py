0e52cf770e221021468809c375f4201ed67f49a5
Louis Woodley <lwoodley@cps.edu>
2023-12-06 11:43:43 -0600

    more work

--- part2-training-testing-data/diabetes.py
+++ part2-training-testing-data/diabetes.py
@@ -1,6 +1,6 @@
-#                                                                                                                                a
+
 import numpy as np
-import matplotlib.pyplot as pl
+import matplotlib.pyplot as plt
 from sklearn.datasets import load_diabetes
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
